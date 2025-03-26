67b90454754bc6b8f0514f21b90bfb72c4c43b2d
Angel <abasurco0@gmail.com>
2025-03-26 11:24:36 +0100

    introduccion de db con kotlin

---
+++ app/src/main/java/com/angel/todolist/Task.kt
@@ -0,0 +1,16 @@
+package com.angel.todolist
+
+class Task (
+    var id: Long,
+    var title: String,
+    var done: Boolean
+
+
+) {
+    companion object {
+        const val COLUMN_NAME_ID = "id"
+        const val COLUMN_NAME_TITLE = "title"
+        const val COLUMN_NAME_DONE = "done"
+        const val TABLE_NAME = "Tasks"
+    }
+}
